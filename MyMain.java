3fd48fcd7ddbeb4b762b7d456839ba3b0e54cc26
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-05-10 08:10:52 +0530

    Design Patterns MyMain

--- MyMain.java
+++ MyMain.java
@@ -1,7 +1,9 @@
 public class MyMain {
     public static void main(String[] args) {
         MySingleton.Start();
+        
         MyBuilder.Start();
+        
         MyObserver.Start();
     }
 }
